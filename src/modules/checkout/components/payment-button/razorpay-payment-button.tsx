848103b36f71021122e176cc4e35a52637378cec
sabarish-p-wailatech <102712900+GuhanGK@users.noreply.github.com>
2025-09-11 13:04:06 +0530

    main
    the issue is fixed

--- src/modules/checkout/components/payment-button/razorpay-payment-button.tsx
+++ src/modules/checkout/components/payment-button/razorpay-payment-button.tsx
@@ -49,7 +49,8 @@ export const RazorpayPaymentButton = ({
     const options: RazorpayOrderOptions = {
       key:process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID??process.env.NEXT_PUBLIC_RAZORPAY_TEST_KEY_ID??"your_key_id",
       callback_url: `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/razorpay/hooks`,
-      amount: session.amount*100*100,
+      // amount: session.amount*100*100,
+      amount: session.amount * 100,
       order_id: orderData.razorpayOrder.id,
       currency: cart.currency_code.toUpperCase() as CurrencyCode,
       name: process.env.COMPANY_NAME ?? "your company name ",
